03010e83833dc5cf5e1c8e89683868319b46d4ad
xulp <Xu_Liangpan@163.com>
2020-03-17 22:18:33 +0800

    feat:hash match

--- library/two_sum/two_sum.go
+++ library/two_sum/two_sum.go
@@ -1,6 +1,6 @@
 package two_sum
 
-func two_sum(nums []int, target int) []int {
+func TwoSumByFor(nums []int, target int) []int {
 	result := make([]int, 2)
 	for i, num := range nums {
 		aim := target - num
@@ -10,6 +10,36 @@ func two_sum(nums []int, target int) []int {
 				result[1] = j
 			}
 		}
+	}
+	return result
+}
+
+func TwoSumByHash(nums []int, target int) []int {
+	numsMap := map[int]int{}
+	result := make([]int, 2)
+	for i, num := range nums {
+		numsMap[num] = i + 1
+	}
+	for i, num := range nums {
+		index := numsMap[target-num]
+		if index != 0 && index != i+1 {
+			result[0] = index - 1
+			result[1] = i
+		}
+	}
+	return result
+}
+
+func TwoSumByHashOnce(nums []int, target int) []int {
+	numsMap := map[int]int{}
+	result := make([]int, 2)
+	for i, num := range nums {
+		index := numsMap[target-num]
+		if index != 0 && index != i+1 {
+			result[0] = index - 1
+			result[1] = i
+		}
+		numsMap[num] = i + 1
 	}
 	return result
 }
--- library/two_sum/two_sum_test.go
+++ library/two_sum/two_sum_test.go
@@ -9,20 +9,49 @@ import (
 var (
 	nums   = []int{2, 7, 11, 15}
 	target = 9
-	expect = []int{1, 0}
+	expect = []int{0, 1}
 )
 
 func TestTwoSum(t *testing.T) {
-	result := two_sum(nums, target)
-	assert.Equal(t, result, expect)
+	t.Run("for", func(t *testing.T) {
+		result := TwoSumByFor(nums, target)
+		assert.Equal(t, result, expect)
+	})
+	t.Run("hash", func(t *testing.T) {
+		result := TwoSumByHash(nums, target)
+		assert.Equal(t, result, expect)
+	})
+	t.Run("hashOnce", func(t *testing.T) {
+		result := TwoSumByHash(nums, target)
+		assert.Equal(t, result, expect)
+	})
 }
 
 func BenchmarkTwoSum(b *testing.B) {
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		two_sum(nums, target)
-	}
+	b.Run("for", func(b *testing.B) {
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			TwoSumByFor(nums, target)
+		}
+	})
+	b.Run("hash", func(b *testing.B) {
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			TwoSumByHash(nums, target)
+		}
+	})
+	b.Run("hashOnce", func(b *testing.B) {
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			TwoSumByHashOnce(nums, target)
+		}
+	})
+
 }
 
-// BenchmarkTwoSum-4   	25894044	        47.8 ns/op	      16 B/op	       1 allocs/op
+// BenchmarkTwoSum/for-4         	25707518	        47.4 ns/op	      16 B/op	       1 allocs/op
+// BenchmarkTwoSum/hash-4        	 8463904	       140 ns/op	      16 B/op	       1 allocs/op
+// BenchmarkTwoSum/hashOnce-4    	 8723658	       135 ns/op	      16 B/op	       1 allocs/op
