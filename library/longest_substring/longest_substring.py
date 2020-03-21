0962e860fe195359768a4eaea834b9ffefab598f
xulp <Xu_Liangpan@163.com>
2020-03-21 21:59:56 +0800

    feat: longest substring

---
+++ library/longest_substring/longest_substring.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/21 下午8:31
+# @Author  : serendipity-xp
+# @FileName: longest_substring.py
+# @Software: PyCharm
+# @GitHub  ：https://github.com/serendipity-xp
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        longest_substring = []
+        max_length = 0
+        for character in s:
+            if character in longest_substring:
+                longest_substring = longest_substring[longest_substring.index(character)+1:]
+            longest_substring.append(character)
+            max_length = max(max_length, len(longest_substring))
+        return max_length
+
+
+if __name__ == '__main__':
+    solution_obj = Solution()
+    test_str = 'pwwkew'
+    print solution_obj.lengthOfLongestSubstring(test_str)
