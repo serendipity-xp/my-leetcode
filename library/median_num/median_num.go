75ff10517b4bd50331c4495cb80bbb0247c97eca
xulp <Xu_Liangpan@163.com>
2020-03-27 21:45:16 +0800

    fix:code work on [] and [1]

--- library/median_num/median_num.go
+++ library/median_num/median_num.go
@@ -8,8 +8,13 @@ func GetMedianNum(list1, list2 []int) float64 {
 	list1 = append(list1, list2...)
 	sort.Ints(list1)
 	length := len(list1)
-	if length%2 == 0 {
-		return float64(list1[length/2] + list1[length/2-1]) / 2
+	if length == 0 {
+		return 0
+	} else if length == 1 {
+		return float64(list1[0]) / 2
+	} else if length%2 == 0 {
+		return float64(list1[length/2]+list1[length/2-1]) / 2
+	} else {
+		return float64(list1[length/2])
 	}
-	return float64(list1[length/2])
 }
--- library/median_num/median_num_test.go
+++ library/median_num/median_num_test.go
@@ -1,16 +1,25 @@
 package median_num
 
 import (
-	`testing`
+	"testing"
 
-	`github.com/stretchr/testify/assert`
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetMedianNum(t *testing.T) {
-	var(
-		list1 = []int{3,1,0,1}
-		list2 = []int{2,2,3,1}
+	var (
+		list1 = []int{3, 1, 0, 1}
+		list2 = []int{2, 2, 3, 1}
+
+		list3 []int
+		list4 []int
+
+		list5 = []int{1}
 	)
 	medianNum := GetMedianNum(list1, list2)
-	assert.Equal(t,1.5,medianNum)
+	assert.Equal(t, 1.5, medianNum)
+	medianNum = GetMedianNum(list3, list4)
+	assert.Equal(t, 0.0, medianNum)
+	medianNum = GetMedianNum(list4, list5)
+	assert.Equal(t, 0.5, medianNum)
 }
