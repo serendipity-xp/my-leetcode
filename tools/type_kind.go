3ef4be41aca5091c6b21134da77c660d91f42a21
徐梁攀 <1057262993@qq.com>
2020-07-07 14:39:25 +0800

    Feat: 数据类型枚举

--- tools/type_kind.go
+++ tools/type_kind.go
@@ -1,1 +1,33 @@
 package tools
+
+type Kind uint
+
+const (
+	Invalid Kind = iota
+	Bool
+	Int
+	Int8
+	Int16
+	Int32
+	Int64
+	Uint
+	Uint8
+	Uint16
+	Uint32
+	Uint64
+	Uintptr
+	Float32
+	Float64
+	Complex64
+	Complex128
+	Array
+	Chan
+	Func
+	Interface
+	Map
+	Ptr
+	Slice
+	String
+	Struct
+	UnsafePointer
+)
