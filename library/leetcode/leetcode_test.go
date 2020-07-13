fa226b7c77a5409333b0d7b3685670b53be88ef0
徐梁攀 <1057262993@qq.com>
2020-07-13 09:22:23 +0800

    Feat:两数之和

--- library/leetcode/leetcode_test.go
+++ library/leetcode/leetcode_test.go
@@ -1,1 +1,16 @@
 package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Leetcode(t *testing.T) {
+	t.Run("两数之和", func(t *testing.T) {
+		nums := []int{0, -2, 0,2,-5} // [-1,-2,-3,-4,-5] -8
+		target := 0
+
+		result := twoSumHash(nums, target)
+		fmt.Println(result)
+	})
+}
--- library/leetcode/两数之和.go
+++ library/leetcode/两数之和.go
@@ -1,1 +1,32 @@
 package leetcode
+
+func twoSum(nums []int, target int) []int {
+	result := make([]int,2)
+	for index1, value1 := range nums{
+		// if value1 <= target{
+			other := target - value1
+			for index2 := index1 + 1; index2 < len(nums); index2++{
+				if other == nums[index2]{
+					result[0] = index1
+					result[1] = index2
+				}
+			}
+		}
+	// }
+	return result
+}
+
+func twoSumHash(nums []int, target int) []int {
+	result := make([]int,2)
+	hashMap := make(map[int]int)
+	for index, value := range nums{
+		other := target - value
+		if otherIndex, exist := hashMap[other]; exist {
+			result[0] = otherIndex
+			result[1] = index
+			break
+		}
+		hashMap[value] = index
+	}
+	return result
+}
