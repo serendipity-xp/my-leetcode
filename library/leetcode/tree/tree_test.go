87f11429c437720ea6e6a117bd06bb0c7677ee8b
xuliangpan <Xu_Liangpan@163.com>
2020-07-18 17:10:55 +0800

    Feat: 相同的树

--- library/leetcode/tree/tree_test.go
+++ library/leetcode/tree/tree_test.go
@@ -2,8 +2,62 @@ package tree
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"my-leetcode/datastructure/tree"
 )
 
 func TestTree(t *testing.T) {
-
+	treeObj1 := &tree.TreeNode{
+		Val: 5,
+		Left: &tree.TreeNode{
+			Val: 3,
+			Left: &tree.TreeNode{
+				Val: 2,
+				Left: &tree.TreeNode{
+					Val: 1,
+				},
+			},
+			Right: &tree.TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 7,
+			Left: &tree.TreeNode{
+				Val: 6,
+			},
+			Right: &tree.TreeNode{
+				Val: 8,
+			},
+		},
+	}
+	treeObj2 := &tree.TreeNode{
+		Val: 5,
+		Left: &tree.TreeNode{
+			Val: 3,
+			Left: &tree.TreeNode{
+				Val: 2,
+				Left: &tree.TreeNode{
+					Val: 1,
+				},
+			},
+			Right: &tree.TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &tree.TreeNode{
+			Val: 7,
+			Left: &tree.TreeNode{
+				Val: 6,
+			},
+			Right: &tree.TreeNode{
+				Val: 9,
+			},
+		},
+	}
+	t.Run("相同的树", func(t *testing.T) {
+		assert.Equal(t, isSameTree(treeObj1, treeObj2), false)
+	})
 }
